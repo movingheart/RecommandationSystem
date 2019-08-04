981f754c5fb6c13d5ebca373856b5aa5323d77fc
movingheart <movingheart000@gmail.com>
2019-08-04 14:17:01 +0800

    完成基尼系数的计算

--- chapter-one/gini_index.py
+++ chapter-one/gini_index.py
@@ -4,6 +4,7 @@ Email: wanshitao@donews.com
 Date: 19-8-3 下午7:46
 """
 
+import operator
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -33,25 +34,39 @@ def gen_data():
 
 def plot_gini(my_df):
     plt.figure()
-    my_df['num_cumsum'] = [i/10 for i in range(1,11)]
+    my_df['num_cumsum'] = [i/10 for i in range(1, 11)]
     my_df.sort_values(by='prob')
     my_df['cumsum'] = my_df['prob'].cumsum()
     my_df.plot(x='num_cumsum', y='cumsum', color='blue')
 
-
-    plt.plot(my_df.loc[[0,9],['num_cumsum']]['num_cumsum'],
+    plt.plot(my_df.loc[[0, 9], ['num_cumsum']]['num_cumsum'],
              my_df.loc[[0, 9], ['cumsum']]['cumsum'],
              color='r')
 
     plt.fill(my_df['num_cumsum'], my_df['cumsum'], color='grey')
     plt.fill_between(my_df['num_cumsum'], my_df['cumsum'], color='green')
     plt.xlabel("商品个数累计占比")
-    plt.ylabel("流行度数累计占比")
+    plt.ylabel("流行度累计占比")
     plt.text(0.7, 0.4, "A", fontdict={'size': 16, 'color': 'r'})
     plt.text(0.9, 0.1, "B", fontdict={'size': 16, 'color': 'r'})
-    plt.show()
-    print(my_df)
+    plt.savefig("gini_inde.jpg", dpi=300)
+    # plt.show()
+
+
+def cal_gini(my_df):
+    # 转换为符合如下公式的数据结构
+    p = my_df['prob'].to_dict()
+    j = 1
+    n = len(p)
+    G = 0
+    for item, weight in sorted(p.items(), key=operator.itemgetter(1)):
+        G += (2 * j - n - 1) * weight
+        j += 1
+    return G/n
+
 
 if __name__ == "__main__":
     df = gen_data()
+    print("原始数据：\n", df)
     plot_gini(df)
+    print("基尼系数：", cal_gini(df))
